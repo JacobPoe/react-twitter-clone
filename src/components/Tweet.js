605c1f0386c62ad7544d68db394cbb24392f04c4
Jake Poe <jacobpoe@gmail.com>
2023-09-28 18:41:42 -0400

    initial tweet data setup

---
+++ src/components/Tweet.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { formatTweet } from '../utils/helpers';
+
+const Tweet = (props) => {
+  if (props.tweet === null) {
+    return <p>This Tweet doesn't exist</p>;
+  }
+  return <div className="tweet"></div>;
+};
+
+const mapStateToProps = ({ authedUser, users, tweets }, { id }) => {
+  const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
+
+  return {
+    authedUser,
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
+  };
+};
+
+export default connect(mapStateToProps)(Tweet);
